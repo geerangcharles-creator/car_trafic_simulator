bf2167c6c6c3ca7d23a0cb74be1c302a7d24a0cf
charles <geerangcharles@gmail.com>
2026-06-18 19:34:05 +0200

    ajout de paremetre et methode

--- traffic/TrafficLightSystem.py
+++ traffic/TrafficLightSystem.py
@@ -5,17 +5,47 @@ class TrafficLightSystem:
 
     def __init__(self, mbFeu :TrafficLight, mtFeu : TrafficLight, llFeu : TrafficLight, lrFeu: TrafficLight):
         print("Initialisation du système de feux de circulation")
+        #sauvegardation des feux
+        self.mbFeu = mbFeu
+        self.mtFeu = mtFeu
+        self.llFeu = llFeu
+        self.lrFeu = lrFeu
+        
         self.initializeTrafficLights()
         self.launchTrafficLightSystem()
         self.drawTrafficLights()
-    
+        self.currentState="MAIN_GREEN"
+        
+    #ajout de la methode
+    def switchState(self):
 
-    def drawTrafficLights(self):
+      if self.currentState == "MAIN_GREEN":
+
+        self.cautionMainTraffic()
+        self.currentState = "MAIN_YELLOW"
+
+      elif self.currentState == "MAIN_YELLOW":
+
+        self.authorizeLateralTraffic()
+        self.currentState = "LATERAL_GREEN"
+
+      elif self.currentState == "LATERAL_GREEN":
+
+        self.cautionLateralTraffic()
+        self.currentState = "LATERAL_YELLOW"
+
+      else:
+
+        self.authorizeMainTraffic()
+        self.currentState = "MAIN_GREEN"
+    
+    #root.after(...)
+    def drawTrafficLights(self,canvas=None):
         print("Affichage des feux de circulation")
-        self.mbFeu.drawLight()
-        self.mtFeu.drawLight()
-        self.llFeu.drawLight()
-        self.lrFeu.drawLight()
+        self.mbFeu.drawLight(canvas)
+        self.mtFeu.drawLight(canvas)
+        self.llFeu.drawLight(canvas)
+        self.lrFeu.drawLight(canvas)
 
     
     def initializeTrafficLights(self):
@@ -56,5 +86,13 @@ class TrafficLightSystem:
 
     def launchTrafficLightSystem(self):
         print("Lancement du système de feux de circulation")
-        # un algo pour swifter entre les feux de circulation, mais pour l'instant on va juste afficher un message
-        
+        self.currentState="MAIN_GREEN"
+      
+     #ajout de methode   
+    def isMainTrafficAuthorized(self):
+
+        return self.currentState == "MAIN_GREEN"
+       
+    def isLateralTrafficAuthorized(self):
+
+        return self.currentState == "LATERAL_GREEN"    
