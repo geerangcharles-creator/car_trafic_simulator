7f4d266de8933f433428d8fcfcf3f6ef2397225b
charles <geerangcharles@gmail.com>
2026-06-18 18:55:18 +0200

    ajout de methodes et des fonction pour voiture

--- traffic/Simulation.py
+++ traffic/Simulation.py
@@ -1,4 +1,5 @@
 from traffic import RoadSystem, TrafficLightSystem
+from car import Car
 
 
 class Simulation:
@@ -6,11 +7,45 @@ class Simulation:
         print("Initialisation de la simulation")
         self.trafficLightSystem = TrafficLightSystem()
         self.roadSystem = RoadSystem()  
-        self.generateRandomly4CarsEachSecondInEachDirection(5)  # Génération aléatoire de 5 voitures par seconde dans chaque direction
+        self.generateRandomly4CarsEachSecondInEachDirection(5)# Génération aléatoire de 5 voitures par seconde dans chaque direction
+        self.cars=[]
+        #avoir deja des voiture sur la route 
+        for i in range(5):
+         car = Car(None, None, None, None, None)
+
+         car.x = 390
+         car.y = i * 100
+
+         self.cars.append(car)
 
     
 
 
     def generateRandomly4CarsEachSecondInEachDirection(self, numberOfCars): 
         print("Génération aléatoire de", numberOfCars, "voitures par seconde dans chaque direction")
-        # un algo pour générer aléatoirement des voitures dans chaque direction, mais pour l'instant on va juste afficher un message
+        # un algo pour générer aléatoirement des voitures dans chaque direction
+        for i in range(numberOfCars):
+
+         car = Car(None, None, None, None, None)
+
+         car.x = 390
+         car.y = -(i * 50)
+
+        self.cars.append(car)
+         
+    #tkinter fera appelle a ca    
+    def update(self):
+
+     for car in self.cars:
+        car.move()
+    
+    #suppression de voiture
+     for car in self.cars[:]:
+        car.move()
+        if car.isOutOfScreen():
+            self.cars.remove(car)
+    
+    #methode pour dessiner
+    def drawCars(self, canvas):
+     for car in self.cars:
+            car.draw(canvas)
