b3edc1bd4088c4f35bba47b4c117b35f2ba921c6
charles <geerangcharles@gmail.com>
2026-06-18 21:50:23 +0200

    correction de bougue de fichier roadsystem

--- traffic/RoadSystem.py
+++ traffic/RoadSystem.py
@@ -2,47 +2,31 @@ class RoadSystem:
 
     def __init__(self): 
         print("Initialisation du système routier")
-        #ajout des dimensions des routes
         self.roadwidth = 100 
         self.canvaswidth = 800
         self.canvasheight = 800
-       
-        self.drawRoads()
 
-    def drawRoads(self,canvas=None): 
+    def drawRoads(self, canvas=None): 
         print("Affichage des routes")
-        #ajout de dessin des routes
         if canvas is not None:
+            # Route Horizontale
             canvas.create_rectangle(
-                0,
-                350, 
-                800,
-                450,
-                fill="gray")
-            canvas.create_rectangle( 
-                0,
-                350,
-                800,
-                450,
-                fill="gray"
+                0, 350, 800, 450,
+                fill="gray", outline=""
             )
+            # Route Verticale
+            canvas.create_rectangle(
+                350, 0, 450, 800,
+                fill="gray", outline=""
+            )
+            # Marquages au sol
+            self.drawRoadMarks(canvas)
     
-    def drawRoadMarks(self,canvas=None):
-        
-        canvas.create_line(
-            400,
-            0,
-            400,
-            350,
-            fill="white",
-            dash=(20,20)
-        )
-        
-        canvas.create_line(
-            400,
-            450,
-            400,
-            800,
-            fill="white",
-            dash=(20,20)
-        )
+    def drawRoadMarks(self, canvas=None):
+        if canvas is not None:
+            # Lignes pointillées verticales
+            canvas.create_line(400, 0, 400, 350, fill="white", dash=(20, 20))
+            canvas.create_line(400, 450, 400, 800, fill="white", dash=(20, 20))
+            # Lignes pointillées horizontales
+            canvas.create_line(0, 400, 350, 400, fill="white", dash=(20, 20))
+            canvas.create_line(450, 400, 800, 400, fill="white", dash=(20, 20))
