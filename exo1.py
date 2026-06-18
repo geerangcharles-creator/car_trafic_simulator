c5ef029fb760d1c569fd28592c78137475609ce4
Emlink Deploy <deploy@emlinkapp.com>
2026-06-18 10:36:11 +0100

    Premier TP done

---
+++ exo1.py
@@ -0,0 +1,48 @@
+
+
+color = "red"
+width = 20
+height = 60
+speed = 25 # m/s
+
+
+def printCar():
+    print("Car color:", color)
+    print("Car width:", width)
+    print("Car height:", height)
+    print("Car speed:", speed, "m/s")
+
+def changeSpeed(vitesse):
+    global speed
+    speed = vitesse
+    print("New speed:", speed, "m/s")   
+
+
+printCar()
+changeSpeed(0)
+changeSpeed(100)
+
+
+
+redLight  = "object avec une couleur rouge vif ou rouge sombre "
+greenLight  = "object avec une couleur verte vif ou verte sombre "
+yellowLight  = "object avec une couleur jaune vif ou jaune sombre "
+
+greenOrRedTime = 30
+yellowTime = 5
+
+def authorizeLight():
+    global redLight, greenLight, yellowLight
+    greenLight = "mettre une couleur verte vive"
+    redLight = "mettre une couleur rouge sombre"
+    yellowLight = "mettre une couleur jaune sombre"
+
+    print("Feu vert") 
+
+
+def stopLight():
+    global redLight, greenLight, yellowLight
+    greenLight = "mettre une couleur verte sombre"
+    redLight = "mettre une couleur rouge vive"
+    yellowLight = "mettre une couleur jaune sombre"
+    print("Feu rouge")
