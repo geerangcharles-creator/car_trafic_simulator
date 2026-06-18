f3b34282bb08388133b4f05bce55b400c8a44ec3
charles <geerangcharles@gmail.com>
2026-06-18 19:17:18 +0200

    ajout des methode et des element manquant

--- traffic/TrafficLight.py
+++ traffic/TrafficLight.py
@@ -4,34 +4,80 @@ class TrafficLight:
 
     def __init__(self, redLight, greenLight, yellowLight):
         self.__color = "Red"
+        self.redLight = redLight
+        self.greenLight = greenLight
+        self.yellowLight = yellowLight
+        self.x= 500
+        self.y= 300
+
     
 
-    def drawLight(self):
+    def drawLight(self,canvas=None):
         print("Affichage du feu de circulation")
         print("Couleur du feu : ", self.__color)
         print("Durée du feu vert : ", self.greenOrRedTime, " secondes")
         print("Durée du feu rouge : ", self.greenOrRedTime, " secondes")
         print("Durée du feu jaune : ", self.yellowTime, " secondes")
+        
+        if canvas is not None:
+            canvas.create_rectangle(
+                self.x,
+                self.y,
+                self.x +40,
+                self.y +120,
+                fill="black"
+            )
+            canvas.create_oval(
+                self.x + 10,
+                self.y + 10,
+                self.x + 30,
+                self.y + 30,
+                fill=self.redLight
+            )
+            canvas.create_oval(
+                 self.x + 10,
+                 self.y + 50,
+                 self.x + 30,
+                 self.y + 70,
+                 fill=self.yellowLight
+            )
+            canvas.create_oval(
+                self.x + 10,
+                self.y + 90,
+                self.x + 30,
+                self.y + 110,
+                fill=self.greenLight
+            )
+
 
     def authorizeLight(self): 
-        self.greenLight = "mettre une couleur verte vive"
-        self.redLight = "mettre une couleur rouge sombre"
-        self.yellowLight = "mettre une couleur jaune sombre"
+        self.greenLight = "green"
+        self.redLight = "darkred"
+        self.yellowLight = "goldenrod4"
+        self.__color = "Green"
         print("Feu vert") 
+        
+       
 
 
     def stopLight(self): 
-        self.greenLight = "mettre une couleur verte sombre"
-        self.redLight = "mettre une couleur rouge vive"
-        self.yellowLight = "mettre une couleur jaune sombre"
+        self.greenLight = "darkgreen"
+        self.redLight = "red"
+        self.yellowLight = "goldenrod4"
+        self.__color = "Red"
         print("Feu rouge")
 
 
     def cautionLight(self): 
-        self.greenLight = "mettre une couleur verte sombre"
-        self.redLight = "mettre une couleur rouge sombre"
-        self.yellowLight = "mettre une couleur jaune vive"
-
+        self.greenLight = "darkgreen"
+        self.redLight = "darkred"
+        self.yellowLight = "yellow"
+        self.__color = "Yellow"
+        print("Feu jaune")
+    #une nouvelle methode    
+    def getColor(self):
+         return self.__color
+    
         ## Ici le jaune doit clignoter, 
         # mais pour l'instant on ne peut pas le faire en console, donc on va juste afficher un message
-        print("Feu jaune")
+        
